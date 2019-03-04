0239c58ef22adc4af7cf4a3cf02954cc4d0c6b85
Jake Malley <jja.malley@gmail.com>
2019-03-04 22:36:41 +0000

    Cropped Thumbs to square.

--- src/plugins/navbar.js
+++ src/plugins/navbar.js
@@ -5,23 +5,23 @@ export default {
   buttons: [
     {
       name: 'Home',
-      routeName: 'Welcome',
-      icon: 'mdi-home'
+      routeName: 'Welcome'
+    },
+    {
+      name: 'Gallery',
+      routeName: 'Gallery'
     },
     {
       name: 'Projects',
-      routeName: 'Projects',
-      icon: 'mdi-folder-multiple-outline'
+      routeName: 'Projects'
     },
     {
       name: 'About',
-      routeName: 'About',
-      icon: 'mdi-information-outline'
+      routeName: 'About'
     },
     {
       name: 'Contact Me',
-      routeName: 'Contact',
-      icon: 'mdi-account-box-outline'
+      routeName: 'Contact'
     }
   ]
 }
